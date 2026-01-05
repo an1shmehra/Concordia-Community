a12609a0f2d8bdec23eaa807053e18897eba6f22
an1shmehra <mehraanishh@gmail.com>
2026-01-05 17:44:51 -0500

    Add PostgreSQL support and secure API configuration

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostRepository extends JpaRepository<RedditPost, Long> {
+public interface
+        PostRepository extends JpaRepository<RedditPost, Long> {
     Optional<RedditPost> findByRedditId(String redditId);
 }
